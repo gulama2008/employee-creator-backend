c2b786648f620009a8af4a648fbd0b80d346dc8a
Siyu Liu <siyu.java.web@gmail.com>
2023-11-13 17:38:57 +1100

    init repo

---
+++ src/test/java/io/nology/siyuliu/employeecreatorbackend/EmployeeCreatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.siyuliu.employeecreatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCreatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
